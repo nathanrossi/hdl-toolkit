91f6ea3a5c9c8be623ef0b65ce09bde96489c398
Nathan Rossi <nathan@nathanrossi.com>
2011-12-11 18:13:08 +1000

    CoreXiseGenCommand support for selecting the device in the project.

--- src/HDLToolkit.Console/Commands/CoreXiseGenCommand.cs
+++ src/HDLToolkit.Console/Commands/CoreXiseGenCommand.cs
@@ -19,6 +19,8 @@ using System.Text;
 using NConsole;
 using HDLToolkit.Xilinx;
 using System.IO;
+using HDLToolkit.Framework.Devices;
+using HDLToolkit.Console.Helpers;
 
 namespace HDLToolkit.Console.Commands
 {
@@ -31,6 +33,9 @@ namespace HDLToolkit.Console.Commands
 		[Argument(ShortName = "u", LongName = "ucf")]
 		public string UserConstraintsFile { get; set; }
 
+		[Argument(ShortName = "d", LongName = "device")]
+		public string DeviceQueryString { get; set; }
+
 		[Argument(Position = 0)]
 		public string[] Cores { get; set; }
 
@@ -41,6 +46,26 @@ namespace HDLToolkit.Console.Commands
 			if (!string.IsNullOrEmpty(OutputPath))
 			{
 				XilinxProjectFile prj = new XilinxProjectFile(Program.Repository);
+
+				if (!string.IsNullOrEmpty(DeviceQueryString))
+				{
+					DeviceManager manager = new DeviceManager();
+					XilinxDeviceTree devTree = new XilinxDeviceTree();
+					devTree.Load();
+					manager.Manufacturers.Add(devTree);
+
+					prj.Device = GetPart(manager, DeviceQueryString);
+					if (prj.Device == null)
+					{
+						Logger.Instance.WriteError("Could not found a device to match the string '{0}'", DeviceQueryString);
+						return;
+					}
+					else
+					{
+						Logger.Instance.WriteVerbose("Selected device '{0}'", prj.Device.Name);
+					}
+				}
+				
 				if (!string.IsNullOrEmpty(UserConstraintsFile))
 				{
 					prj.UserConstraintsFile = Path.GetFullPath(UserConstraintsFile);
@@ -60,7 +85,35 @@ namespace HDLToolkit.Console.Commands
 			else
 			{
 				Logger.Instance.WriteError("Output Path not specified, terminating...");
+				return;
 			}
+		}
+
+		private static DevicePartSpeed GetPart(DeviceManager manager, string query)
+		{
+			object o = DeviceHelper.FindDevice(manager, query);
+			
+			Device device = o as Device;
+			if (device != null)
+			{
+				if (device.Parts.Count != 0)
+				{
+					Logger.Instance.WriteWarning("Found device, unable to match with package specifier, selecting default package '{0}'", device.Parts[0].Package.Name);
+					o = device.Parts[0];
+				}
+			}
+
+			DevicePart part = o as DevicePart;
+			if (part != null)
+			{
+				if (part.Speeds.Count != 0)
+				{
+					Logger.Instance.WriteWarning("Found device, unable to match with speed specifier, selecting default speed '{0}'", part.Speeds[0].Speed.Name);
+					o = part.Speeds[0];
+				}
+			}
+
+			return (o as DevicePartSpeed);
 		}
 	}
 }
---
+++ src/HDLToolkit.Console/Helpers/DeviceHelper.cs
@@ -0,0 +1,66 @@
+﻿// Copyright 2011 Nathan Rossi - http://nathanrossi.com
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HDLToolkit.Framework.Devices;
+
+namespace HDLToolkit.Console.Helpers
+{
+	public static class DeviceHelper
+	{
+		public static object FindDevice(DeviceManager manager, string query)
+		{
+			IEnumerable<object> parts = manager.FindPart(query);
+			Logger.Instance.WriteDebug("Found {0} matching parts", parts.Count());
+
+			Device topDevice = null;
+			DevicePart topDevicePart = null;
+			DevicePartSpeed topDevicePartSpeed = null;
+			foreach (object o in parts)
+			{
+				if (o is Device)
+				{
+					topDevice = o as Device;
+				}
+				else if (o is DevicePart)
+				{
+					topDevicePart = o as DevicePart;
+				}
+				else if (o is DevicePartSpeed)
+				{
+					topDevicePartSpeed = o as DevicePartSpeed;
+					// pick the first one of these
+					break;
+				}
+			}
+
+			if (topDevicePartSpeed != null)
+			{
+				return topDevicePartSpeed;
+			}
+			if (topDevicePart != null)
+			{
+				return topDevicePart;
+			}
+			if (topDevice != null)
+			{
+				return topDevice;
+			}
+			return null;
+		}
+	}
+}
