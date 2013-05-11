3b15e96d481424d1c867898571a793fc48d5ccf6
Nathan Rossi <nathan@nathanrossi.com>
2013-05-11 20:20:18 +1000

    IToolchain: Create Toolchain interface

---
+++ src/HDLToolkit/Framework/IToolchain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HDLToolkit.Framework.Synthesis;
+using HDLToolkit.Framework.Simulation;
+using HDLToolkit.Framework.Implementation;
+
+namespace HDLToolkit.Framework
+{
+	public interface IToolchain
+	{
+		ToolchainManager Manager { get; }
+		IToolchainVersion Version { get; }
+		string UniqueId { get; }
+
+		IEnumerable<ISimulator> Simulators { get; }
+		IEnumerable<ISynthesizer> Synthesizers { get; }
+		IEnumerable<IImplementor> Implementors { get; }
+	}
+}
---
+++ src/HDLToolkit/Framework/IToolchainVersion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HDLToolkit.Framework
+{
+	public interface IToolchainVersion
+	{
+		string UniqueId { get; }
+
+		string ToString();
+	}
+}
