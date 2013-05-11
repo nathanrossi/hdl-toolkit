3a9261e70710663d1bd29dae8b21c98ad6d374ac
Nathan Rossi <nathan@nathanrossi.com>
2013-05-11 20:27:51 +1000

    ToolchainManager: Created IToolchain management class
    * Manages multiple IToolchain's
    * Maintains DeviceManager

---
+++ src/HDLToolkit/Framework/ToolchainManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HDLToolkit.Framework.Devices;
+
+namespace HDLToolkit.Framework
+{
+	public class ToolchainManager
+	{
+		public HashSet<IToolchain> Toolchains { get; private set; }
+		public DeviceManager Devices { get; private set; }
+
+		public ToolchainManager()
+		{
+			Toolchains = new HashSet<IToolchain>();
+			Devices = new DeviceManager();
+		}
+
+		public void AddToolchain(IToolchain toolchain)
+		{
+			Toolchains.Add(toolchain);
+		}
+
+		public IToolchain FindToolchainById(string id)
+		{
+			foreach (IToolchain toolchain in Toolchains)
+			{
+				if (string.Compare(toolchain.UniqueId, id, true) == 0)
+				{
+					return toolchain;
+				}
+			}
+			return null;
+		}
+	}
+}
---
+++ src/HDLToolkit/Framework/ToolchainReference.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace HDLToolkit.Framework
+{
+	public class ToolchainReference : IXmlSerializable
+	{
+		public string Id { get; private set; }
+
+		public bool IsNull
+		{
+			get { return string.IsNullOrEmpty(Id); }
+		}
+
+		public ToolchainReference()
+		{
+		}
+
+		public ToolchainReference(string id)
+		{
+			Id = id;
+		}
+
+		public ToolchainReference(IToolchain toolchain)
+		{
+			Id = toolchain.UniqueId;
+		}
+
+		public IToolchain Toolchain(ToolchainManager manager)
+		{
+			return manager.FindToolchainById(Id);
+		}
+
+		public bool Match(IToolchain toolchain)
+		{
+			return (string.Compare(toolchain.UniqueId, Id, true) == 0);
+		}
+
+		public bool Match(ToolchainReference reference)
+		{
+			return (string.Compare(reference.Id, Id, true) == 0);
+		}
+
+		public virtual XElement Serialize()
+		{
+			XElement toolchainElement = new XElement("toolchain");
+			toolchainElement.Add(new XAttribute("id", Id));
+
+			return toolchainElement;
+		}
+
+		public virtual void Deserialize(XElement element)
+		{
+			if (string.Compare(element.Name.ToString(), "toolchain") == 0)
+			{
+				XAttribute toolchainId = element.Attribute("id");
+				if (toolchainId != null)
+				{
+					Id = toolchainId.Value;
+				}
+			}
+		}
+	}
+}
